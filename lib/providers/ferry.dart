0294ec5b1bef8968d0d77dced4c4581ff765e99b
525UmerYousaf <umer.yousaf.587>
2023-05-14 04:42:39 +0500

    I am establishing client for ferry graphQL, work is remaining

---
+++ lib/providers/ferry.dart
@@ -0,0 +1,21 @@
+import 'package:ferry/ferry.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gql_http_link/gql_http_link.dart';
+
+import './auth.dart';
+
+final ferryClientProvider = Provider<Client>((ref) {
+  final auth = ref.watch(authProvider);
+  final token = auth.token ?? dotenv.env["FAUNA_PUBLIC_KEY"];
+
+  print('>>> token: $token');
+
+  return Client(
+    link: HttpLink("https://graphql.fauna.com/graphql", defaultHeaders: {
+      'Authorization': 'Bearer $token',
+    }),
+  );
+});
+
+//  2.26
